e32df00d5b3503823c42bbbe28f88c1571260c91
gigyboys <severingigy@yahoo.fr>
2017-01-09 09:34:11 +0300

    Modification dropdown menu

--- web/assets/js/button.js
+++ web/assets/js/button.js
@@ -1,36 +1,36 @@
 $(function() {
-    var menu_deroulant_button = $('.menu_deroulant_button');
-    var menu_deroulant_liste = $('.menu_deroulant_liste');
+    var dd_src = $('.dd_src');
+    var dd_target = $('.dd_target');
     var link = $('.link');
     
     $(document).click(function() {
-        var menu_deroulant = $('.menu_deroulant_liste');
-        menu_deroulant.css('display','none');
+        var dd_target = $('.dd_target');
+        dd_target.hide();
     });
     
-    menu_deroulant_button.live('click', function(e) {
-        
+    dd_src.live('click', function(e) {
+        var $this = $(this);
         e.stopPropagation(); 
         e.preventDefault(true);
         
-        var menu_deroulant = $('.menu_deroulant_liste');
+        var dd_target = $('.dd_target');
         
-        var menu_deroulant_liste_to = $(this).parent().find(".menu_deroulant_liste");
-        if(menu_deroulant_liste_to.css('display') == 'none'){
-            menu_deroulant.css('display','none');
-            menu_deroulant_liste_to.css('display','block').css('margin-left',0);
-            var decalage = $(window).width() - menu_deroulant_liste_to.width() - menu_deroulant_liste_to.offset().left - 15; 
+        var dd_target_to = $this.closest(".dd").find(".dd_target");
+        if(dd_target_to.css('display') == 'none'){
+            dd_target.hide();
+            dd_target_to.css('display','block').css('margin-left',0);
+            var decalage = $(window).width() - dd_target_to.width() - dd_target_to.offset().left - 15; 
             if (decalage < 0){
-                menu_deroulant_liste_to.css('margin-left',decalage);
+                dd_target_to.css('margin-left',decalage);
             }
         }
         else{
-            menu_deroulant.css('display','none');
-            menu_deroulant_liste_to.css('display','none');
+            dd_target.hide();
+            dd_target_to.hide();
         }
     });
     
-    menu_deroulant_liste.live('click', function(e) {
+    dd_target.live('click', function(e) {
         e.stopPropagation(); 
         return false;      
     });
