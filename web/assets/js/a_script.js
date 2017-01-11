751f7353061a93a8bc007099752d68083c8e92ed
gigyboys <severingigy@yahoo.fr>
2017-01-11 11:44:31 +0300

    création js common.js et migration de code common js

--- web/assets/js/a_script.js
+++ web/assets/js/a_script.js
@@ -1,29 +1,3 @@
-function resetBlocEdit(bloc_editable){
-	var bloc_view = bloc_editable.find(".bloc_view");
-	var bloc_edit = bloc_editable.find(".bloc_edit");
-	var btn_edit = bloc_editable.find(".btn_edit");
-	var btn_save = bloc_editable.find(".btn_save");
-	var btn_reset = bloc_editable.find(".btn_reset");
-	bloc_view.show();
-	bloc_edit.hide();
-	btn_edit.show();
-	btn_save.hide();
-	btn_reset.hide();
-}
-
-function editBlocEdit(bloc_editable){
-	var bloc_view = bloc_editable.find(".bloc_view");
-	var bloc_edit = bloc_editable.find(".bloc_edit");
-	var btn_edit = bloc_editable.find(".btn_edit");
-	var btn_save = bloc_editable.find(".btn_save");
-	var btn_reset = bloc_editable.find(".btn_reset");
-	bloc_view.hide();
-	bloc_edit.show();
-	btn_edit.hide();
-	btn_save.show();
-	btn_reset.show();
-}
-
 $(function() {
 	console.log("init");
 	var body = $('body');
@@ -49,24 +23,6 @@ $(function() {
 	var width_seuil = 800;
 	
 	var ww = $(window);
-	
-	
-	var btn_edit = $('.btn_edit');
-	var btn_reset = $('.btn_reset');
-    
-    btn_edit.click (function (event){
-		//console.log("edit bloc editable");
-        var $this = $(this);
-        var bloc_editable = $this.closest(".bloc_editable");
-		editBlocEdit(bloc_editable);
-    });
-    
-    btn_reset.click (function (event){
-		//console.log("reset edit bloc editable");
-        var $this = $(this);
-        var bloc_editable = $this.closest(".bloc_editable");
-		resetBlocEdit(bloc_editable);
-    });
 	
 	function init(){
 		
