a4908371d9bc3f130bcdf64c3c3b13cf4be5f34d
anthonywii <anthony@wau.solutions>
2017-02-04 15:27:37 +0300

    correction submit

--- web/assets/js/ur.js
+++ web/assets/js/ur.js
@@ -20,9 +20,9 @@ $(function() {
 	
 	//mise a jour de common information.
 	$('#form_ur_common').submit( function(e){
-		e.preventDefault(true);
-		alert("dsfsdf");
-	}
+		e.preventDefault();
+		alert("sumit form_ur_common");
+	});
 	
 	$('#btn_save_ur_common').on('click', function(){
         var $this = $(this);
