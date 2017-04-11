2aa5a6c7e4ed23783b0cdda38e0cbdf5e76ac9f8
gigy <gigy.idbooster@gmail.com>
2017-04-11 12:59:34 +0300

    admin forum : edit topic common

--- src/COM/AdminBundle/Controller/ForumController.php
+++ src/COM/AdminBundle/Controller/ForumController.php
@@ -12,6 +12,7 @@ use COM\UserBundle\Entity\User;
 use COM\ForumBundle\Entity\Topic;
 use COM\ForumBundle\Entity\TopicTranslate;
 use COM\ForumBundle\Entity\Subject;
+use COM\ForumBundle\Form\Type\TopicCommonType;
 
 class ForumController extends Controller
 {
@@ -36,5 +37,43 @@ class ForumController extends Controller
         return $this->render('COMAdminBundle:forum:edit_topic.html.twig', array(
 			'topic' => $topic,
 		));
+    }
+	
+    public function editTopicCommonAction($id, Request $request)
+    {
+		$em = $this->getDoctrine()->getManager();
+		$topicRepository = $em->getRepository('COMForumBundle:Topic');
+        
+        $topic = $topicRepository->find($id);
+        
+		$topicTemp = new Topic();
+		$formTopicCommon = $this->get('form.factory')->create(new TopicCommonType(), $topicTemp);
+		
+        $response = new Response();
+		
+		if ($formTopicCommon->handleRequest($request)->isValid()) {
+			$topic->setDefaultName($topicTemp->getDefaultName());
+			
+			$platformService = $this->container->get('com_platform.platform_service');
+			$slug = $platformService->sluggify($topicTemp->getSlug());
+			
+			$topic->setSlug($slug);
+            
+			$em->persist($topic);
+			
+            $em->flush();
+
+            $response->setContent(json_encode(array(
+                'state' => 1,
+                'defaultName' => $topic->getDefaultName(),
+                'slug' => $topic->getSlug(),
+            )));
+		}else{
+            $response->setContent(json_encode(array(
+                'state' => 0,
+            )));
+		}
+        $response->headers->set('Content-Type', 'application/json');
+		return $response;
     }
 }
---
+++ web/assets/js/a_fm.js
@@ -0,0 +1,39 @@
+
+$(function() {
+	console.log("a_fm.js");
+	
+    $('#btn_save_fm_topic_common').on('click', function(){
+        var $this = $(this);
+		var bloc_editable = $this.closest(".bloc_editable");
+        var target = $this.data('target');
+		console.log(target);
+		var data = {
+			defaultName : bloc_editable.find("#fm_topic_input_defaultname").val(),
+			slug : bloc_editable.find("#fm_topic_input_slug").val()
+		};
+		loadBlocEdit(bloc_editable);
+        $.ajax({
+            type: 'POST',
+            url: target,
+            data: data,
+            dataType : 'json',
+            success: function(data){
+                console.log(data.state);
+				if(data.state){
+					bloc_editable.find("#fm_topic_view_defaultname").text(data.defaultName);
+					bloc_editable.find("#fm_topic_view_slug").text(data.slug);
+					resetBlocEdit(bloc_editable);
+				}
+				else{
+					alert("une erreur est survenue");
+				}
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+				console.log(jqXHR.status);
+				console.log(textStatus);
+				console.log(errorThrown);
+			}
+        });
+    });
+	
+});
